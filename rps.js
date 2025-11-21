365e2582166541171748bc18309a3960d135e29c
Samir Ibrahim <samir.ibrahim@outlook.it>
2025-11-21 14:56:36 +0100

    Change console.log messages into ui messages. Rewrite some code

--- rps.js
+++ rps.js
@@ -26,30 +26,22 @@ function cpuWins(human, cpu) {
   );
 }
 
-function playRound(humanChoice, cpuChoice) {
+function getRoundWinner(humanChoice, cpuChoice) {
   if (humanWins(humanChoice, cpuChoice)) {
-    alert(
-      `You chose: ${humanChoice.toUpperCase()}\nComputer chose: ${cpuChoice.toUpperCase()}\nYou win!`
-    );
     return "player";
   } else if (cpuWins(humanChoice, cpuChoice)) {
-    alert(
-      `You chose: ${humanChoice.toUpperCase()}\nComputer chose: ${cpuChoice.toUpperCase()}\nYou lose!`
-    );
     return "cpu";
   } else {
-    alert(
-      `You chose: ${humanChoice.toUpperCase()}\nComputer chose: ${cpuChoice.toUpperCase()}\nDraw!`
-    );
     return "nowinner";
   }
 }
 
 function checkGameWinner(playerScore, cpuScore) {
+  const h1Winner = document.querySelector("#h1-winner");
   if (playerScore > cpuScore) {
-    console.log("PLAYER WIN THE GAME! :)");
+    h1Winner.textContent = "PLAYER WIN THE GAME! :)";
   } else {
-    console.log("COMPUTER WIN THE GAME! :(");
+    h1Winner.textContent = "CPU WIN THE GAME! :(";
   }
 }
 
@@ -57,32 +49,66 @@ function inputIsValid(text) {
   return text == "rock" || text == "paper" || text == "scissor";
 }
 
+function showPlayerChoice(choice) {
+  let h1PlayerMove = document.querySelector("#h1-player-choice");
+  h1PlayerMove.textContent = `Player chose ${choice}`;
+}
+
 function game() {
-  let round = 1;
   let playerScore = 0;
   let cpuScore = 0;
 
-  while (round <= 5) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    if (inputIsValid(humanSelection)) {
-      console.clear();
-      let winner = playRound(humanSelection, computerSelection);
+  let playerChoice;
+  let cpuChoice;
+  //const humanSelection = getHumanChoice();
+  const btnRock = document.querySelector("#btn-rock");
+  const btnPaper = document.querySelector("#btn-paper");
+  const btnScissor = document.querySelector("#btn-scissor");
+
+  btnRock.addEventListener("click", () => {
+    playerChoice = "rock";
+    showPlayerChoice("rock");
+    playRound();
+  });
+  btnPaper.addEventListener("click", () => {
+    playerChoice = "paper";
+    showPlayerChoice("paper");
+    playRound();
+  });
+  btnScissor.addEventListener("click", () => {
+    playerChoice = "scissor";
+    showPlayerChoice("scissor");
+    playRound();
+  });
+
+  function playRound() {
+    cpuChoice = getComputerChoice();
+    if (inputIsValid(playerChoice)) {
+      let h1CpuMove = document.querySelector("#h1-cpu-choice");
+      let h1Result = document.querySelector("#h1-result");
+      h1CpuMove.textContent = `Computer chose ${cpuChoice}`;
+      let winner = getRoundWinner(playerChoice, cpuChoice);
       if (winner == "cpu") {
         cpuScore++;
-        round++;
+        h1Result.textContent = `${cpuChoice.toUpperCase()} beats ${playerChoice.toUpperCase()}. CPU wins!`;
       } else if (winner == "player") {
         playerScore++;
-        round++;
+        h1Result.textContent = `${playerChoice.toUpperCase()} beats ${cpuChoice.toUpperCase()}. Player wins!`;
+      } else {
+        h1Result.textContent = `Draw!`;
       }
-      console.log(`Player Score: ${playerScore}`);
-      console.log(`Computer Score: ${cpuScore}`);
-    } else {
-      alert("Invalid input!");
+      const h1PlayerScore = document.querySelector("#h1-player-score");
+      const h1CpuScore = document.querySelector("#h1-cpu-score");
+      h1PlayerScore.textContent = `Player Score: ${playerScore}`;
+      h1CpuScore.textContent = `Computer Score: ${cpuScore}`;
+    }
+    if (playerScore + cpuScore == 5) {
+      checkGameWinner(playerScore, cpuScore);
+      btnRock.disabled = true;
+      btnPaper.disabled = true;
+      btnScissor.disabled = true;
     }
   }
-
-  checkGameWinner(playerScore, cpuScore);
 }
 
 game();
